4c3f3f065cdac57f42e2b14d6ec9782d95450d64
avanti <3240103472@zju.edu.cn>
2024-12-29 15:08:44 +0800

    upd build_simu29_2

--- docs/codingprob/memo_align.c
+++ docs/codingprob/memo_align.c
@@ -7,11 +7,19 @@ struct student
     char c;
     double d;
     char e;
-}stu;
+};
 
 int main()
 {
+    struct student stu;
     int a, b;
-    
+
+    stu.a[0] = 'A', stu.a[1] = 'B', stu.a[2] = 'C';
+    for (int i = 0; i < 20;i++)
+        stu.b[i] = i;
+    stu.c = '0';
+    stu.d = 16.0;
+    stu.e = 'a';
+
     return 0;
 }
